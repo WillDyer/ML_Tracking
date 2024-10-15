1e30237ccf354c5c7bc509f84e2828fe60cf1400
WillDyer <will.dyer9@gmail.com>
2024-10-15 11:35:13 +0100

    2 cam support for more accuracy

--- tracking_hou.py
+++ tracking_hou.py
@@ -23,6 +23,7 @@ CUSTOM_POSE_CONNECTIONS = [
 
 show_pose = 1
 show_hands = 1
+show_video_sources = True
 webcam = False
 
 video_source_1 = "/run/media/will/Will_s SSD1/University_Projects/YR3/ML_Tracking/clips/walk_cycle.mp4"
@@ -55,41 +56,79 @@ def get_landmark_data(frame, pose_detector, hand_detector):
         "hand_landmarks": []
     }
 
-    if pose_results.pose_landmarks:
-        for index, landmark in enumerate(pose_results.pose_landmarks.landmark):
-            if index not in EXCLUDED_LANDMARKS:
-                landmarks["pose_landmarks"].append({
-                    "x": landmark.x,
-                    "y": landmark.y,
-                    "z": landmark.z,
-                    "visibility": landmark.visibility
-                })
+    if show_pose == 1:
+        if pose_results.pose_landmarks:
+            for index, landmark in enumerate(pose_results.pose_landmarks.landmark):
+                if index not in EXCLUDED_LANDMARKS:
+                    landmarks["pose_landmarks"].append({
+                        "x": landmark.x,
+                        "y": landmark.y,
+                        "z": landmark.z,
+                        "visibility": landmark.visibility
+                    })
 
-                cx, cy = int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
-                cv2.circle(frame, (cx, cy), 3, (128, 0, 255), thickness=5)
+                    cx, cy = int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
+                    cv2.circle(frame, (cx, cy), 3, (128, 0, 255), thickness=5)
 
-        for connection in CUSTOM_POSE_CONNECTIONS:
-                    start_idx, end_idx = connection
-                    start_landmark = pose_results.pose_landmarks.landmark[start_idx]
-                    end_landmark = pose_results.pose_landmarks.landmark[end_idx]
+            for connection in CUSTOM_POSE_CONNECTIONS:
+                start_idx, end_idx = connection
+                start_landmark = pose_results.pose_landmarks.landmark[start_idx]
+                end_landmark = pose_results.pose_landmarks.landmark[end_idx]
 
-                    start_cx, start_cy = int(start_landmark.x * frame.shape[1]), int(start_landmark.y * frame.shape[0])
-                    end_cx, end_cy = int(end_landmark.x * frame.shape[1]), int(end_landmark.y * frame.shape[0])
+                start_cx, start_cy = int(start_landmark.x * frame.shape[1]), int(start_landmark.y * frame.shape[0])
+                end_cx, end_cy = int(end_landmark.x * frame.shape[1]), int(end_landmark.y * frame.shape[0])
 
-                    cv2.line(frame, (start_cx, start_cy), (end_cx, end_cy), (99, 255, 107), 2)
+                cv2.line(frame, (start_cx, start_cy), (end_cx, end_cy), (99, 255, 107), 2)
 
-    if hand_results.multi_hand_landmarks:
-        for hand_landmarks in hand_results.multi_hand_landmarks:
-            hand_landmark_data = []
-            for landmark in hand_landmarks.landmark:
-                hand_landmark_data.append({
-                    "x": landmark.x,
-                    "y": landmark.y,
-                    "z": landmark.z
-                })
-            landmarks["hand_landmarks"].append(hand_landmark_data)
-    
+    if show_hands == 1:
+        if hand_results.multi_hand_landmarks:
+            for hand_landmarks in hand_results.multi_hand_landmarks:
+                hand_landmark_data = []
+                for landmark in hand_landmarks.landmark:
+                    hand_landmark_data.append({
+                        "x": landmark.x,
+                        "y": landmark.y,
+                        "z": landmark.z
+                    })
+                landmarks["hand_landmarks"].append(hand_landmark_data)
+
+                mp_drawing.draw_landmarks(
+                            frame,
+                            hand_landmarks, 
+                            mp_hands.HAND_CONNECTIONS
+                        )
+        
     return landmarks
+
+def average_landmarks(landmarks_1, landmarks_2):
+    averaged_landmarks = {
+        "pose_landmarks": [],
+        "hand_landmarks": []
+        }
+
+    if landmarks_1["pose_landmarks"] and landmarks_2["pose_landmarks"]:
+        for pose_1, pose_2 in zip(landmarks_1["pose_landmakrs"], landmarks_2["pose_landmarks"]):
+            average = {
+                "x": (pose_1["x"] + pose_2["x"]) / 2,
+                "y": (pose_1["y"] + pose_2["y"]) / 2,
+                "z": (pose_1["z"] + pose_2["z"]) / 2,
+                "visibility": (pose_1["visibility"] + pose_2["visibility"]) / 2
+            }
+            averaged_landmarks["pose_landmarks"].append(average)
+
+    if landmarks_1["hand_landmarks"] and landmarks_2["hand_landmarks"]:
+        for hand_1, hand_2 in zip(landmarks_1["hand_landmarks"], landmarks_2["hand_landmarks"]):
+            average_hand = []
+            for lm1, lm2 in zip(hand_1, hand_2):
+                average = {
+                    "x": (lm1["x"] + lm2["x"]) / 2,
+                    "y": (lm1["y"] + lm2["y"]) / 2,
+                    "z": (lm1["z"] + lm2["z"]) / 2
+                }
+                average_hand.append(average_hand)
+            averaged_landmarks["hand_landmarks"].append(average_hand)
+
+    return averaged_landmarks
 
 
 with mp_pose.Pose() as pose_detector, mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) as hands_detector:
@@ -104,37 +143,20 @@ with mp_pose.Pose() as pose_detector, mp_hands.Hands(min_detection_confidence=0.
         landmarks_1 = get_landmark_data(frame_1, pose_detector, hands_detector)
         landmarks_2 = get_landmark_data(frame_2, pose_detector, hands_detector)
 
+        averaged_landmarks = average_landmarks(landmarks_1, landmarks_2)
+
         all_landmarks_data = {
-            "pose_landmarks": [],
-            "hand_landmarks": []
+            "pose_landmarks": averaged_landmarks["pose_landmarks"],
+            "hand_landmarks": averaged_landmarks["hand_landmarks"]
         }
-
-        if show_pose == 1:
-            if pose_results.pose_landmarks:
-                for connection in CUSTOM_POSE_CONNECTIONS:
-                    start_idx, end_idx = connection
-                    start_landmark = pose_results.pose_landmarks.landmark[start_idx]
-                    end_landmark = pose_results.pose_landmarks.landmark[end_idx]
-
-                    start_cx, start_cy = int(start_landmark.x * image.shape[1]), int(start_landmark.y * image.shape[0])
-                    end_cx, end_cy = int(end_landmark.x * image.shape[1]), int(end_landmark.y * image.shape[0])
-
-                    cv2.line(image, (start_cx, start_cy), (end_cx, end_cy), (99, 255, 107), 2)
-
-
-        if show_hands == 1:
-            if hand_results.multi_hand_landmarks:
-                for hand_landmarks in hand_results.multi_hand_landmarks:
-                    mp_drawing.draw_landmarks(
-                        image,
-                        hand_landmarks, 
-                        mp_hands.HAND_CONNECTIONS
-                    )
 
         json_data = json.dumps(all_landmarks_data)
         sock.sendto(json_data.encode('utf-8'), (UDP_IP, UDP_PORT))
 
-        cv2.imshow("Pose and Hand Tracker (Excluding Fingers)", image)
+        if show_video_sources is True:
+            cv2.imshow("ML Tracking showing Source_1", frame_1)
+            cv2.imshow("ML Tracking showing Source_2", frame_2)
+
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
